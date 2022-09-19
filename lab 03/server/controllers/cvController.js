4230cbd809bcca89bffb6bc95dec568f6ead2827
Parasite-231 <muktadir@iut-dhaka.edu>
2022-09-20 01:46:55 +0600

    data read for skill section

--- lab 03/server/controllers/cvController.js
+++ lab 03/server/controllers/cvController.js
@@ -1,9 +1,8 @@
 const fs = require("fs");
-const getSkills = require("./testa");
 
 const getCV = (req, res) => {
   educations = fs.readFileSync(
-    "C:/Users/pc/Documents/3-1/server programming/labs/190042136_md_muktadir_mazumder/lab 03/server/models/educationData.json",
+    "C:/Users/pc/Documents/3-1/server programming/labs/190042136_md_muktadir_mazumder/lab 03/server/models/educationData",
     {
       encoding: "utf-8",
     }
@@ -16,10 +15,22 @@ const getCV = (req, res) => {
     edus.push(educations[key]);
   }
 
+  skills = fs.readFileSync(
+    "C:/Users/pc/Documents/3-1/server programming/labs/190042136_md_muktadir_mazumder/lab 03/server/models/skillData",
+    { encoding: "utf-8" }
+  );
+
+  skills = JSON.parse(String(skills));
+
+  skillContainer = [];
+
+  for (let key in skills) {
+    skillContainer.push(skills[key]);
+  }
   res.render("cvLayout", {
     name: "Md Muktadir Mazumder",
     educations: edus,
-    skills: getSkills.getSkills.skillContainer,
+    skills: skillContainer,
   });
 };
 
