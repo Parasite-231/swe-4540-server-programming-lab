de47dc10691216813f590870c7a478c79dafa589
Parasite-231 <muktadir@iut-dhaka.edu>
2022-10-23 00:09:17 +0600

    first server-programming lab

---
+++ arrow function/index.js
@@ -0,0 +1,19 @@
+
+
+//normal function
+function sayHello() {
+
+    console.log("Hello senpai")
+    
+}
+
+//arrow function
+const addTwoNumber = (firstNumber,secondNumber) => {
+
+    return firstNumber + secondNumber
+}
+
+sayHello()
+console.log("The result is : " + addTwoNumber(2,3))
+
+
