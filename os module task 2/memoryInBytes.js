cc7b4ca1f8caeea9e5fda9287674d4175652a24b
Parasite-231 <muktadir@iut-dhaka.edu>
2022-10-23 00:10:15 +0600

    first server-programming lab tasks

--- os module task 2/memoryInBytes.js
+++ os module task 2/memoryInBytes.js
@@ -4,8 +4,8 @@ const convertMemoryToGigaBytes = require('./memoryUtils');
 const totalMemory = os.totalmem();
 
 
-console.log("Memory in Bytes : "+totalMemory)
-console.log("Memory in GigaBytes : "+convertMemoryToGigaBytes.convertMemoryToGigabytes(totalMemory))
+console.log("Memory in Bytes => "+totalMemory)
+console.log("Memory in GigaBytes => "+convertMemoryToGigaBytes.convertMemoryToGigabytes(totalMemory))
 //others
 console.log("Operating version : "+os.version())
 console.log("Type : "+os.type())
